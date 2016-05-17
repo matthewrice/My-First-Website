d74c96590158623940e58f1558640dfaa4b1ddc2
Matthew Rice <mtr.rice@gmail.com>
2016-05-16 23:54:33 -0400

    This is one ugly website

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-// alert ("I love bacon")
+var e = document.getElementById("watchme");
+e.addEventListener("animationstart", listener, false);
+e.addEventListener("animationend", listener, false);
+e.addEventListener("animationiteration", listener, false);
+
+e.className = "slidein";
+
+  //I searched every online to figure out how to implement a blinking tag in JavaScript and had no sucess.//
